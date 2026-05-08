12c61028f43535f654d19b9f17bce39612f91179
rahullparmarbtce2024-hue <rahullparmar.btce2024@iar.ac.in>
2026-05-08 21:12:51 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,24 @@
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ALGORITHM: str
 
-    class Config:
-        env_file = ".env"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    CLOUDINARY_CLOUD_NAME: str = ""
+    CLOUDINARY_API_KEY: str = ""
+    CLOUDINARY_API_SECRET: str = ""
+
+    RESEND_API_KEY: str = ""
+    FROM_EMAIL: str = ""
+
+    model_config = ConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 settings = Settings()
